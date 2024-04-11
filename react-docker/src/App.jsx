a21014b29611bf3e473e96d5a58ea7be8291f135
Jayandra Hamal <salonhamal278@gmail.com>
2024-04-11 21:49:25 +0530

    push react-docker in docker hub

--- react-docker/src/App.jsx
+++ react-docker/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Docker is Awesome!!!!</h1>
+      <h1>well! Docker is docker</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
